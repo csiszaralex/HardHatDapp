859c31ea188f2c76ee39a2fa52783ec599eb0314
Csiszár Alex <csiszaralex@gmail.com>
2024-03-03 21:11:51 +0100

    :sparkle: Update code to use web3 instead of ethers

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,50 @@
-import { ethers } from "hardhat";
+import { ethers, web3 } from 'hardhat';
+import artifacts from '../artifacts/contracts/Lock.sol/Lock.json';
 
-async function main() {
+async function maina() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
+  const lockedAmount = ethers.parseEther('0.001');
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
+  const lock = await ethers.deployContract('Lock', [unlockTime], {
     value: lockedAmount,
   });
-
   await lock.waitForDeployment();
-
   console.log(
     `Lock with ${ethers.formatEther(
       lockedAmount
     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
   );
 }
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+  const lockedAmount = web3.utils.toWei('0.001', 'ether');
+  const [deployer] = await web3.eth.getAccounts();
+  const lockContract = new web3.eth.Contract(artifacts.abi);
+  const rawContract = lockContract.deploy({
+    data: artifacts.bytecode,
+    arguments: [unlockTime],
+  });
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
+
+  const lock = await rawContract.send({
+    from: deployer,
+    gasPrice: '10000000000',
+    value: lockedAmount.toString(),
+  });
+
+  console.log(
+    `Lock with ${web3.utils.fromWei(
+      lockedAmount,
+      'ether'
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.options.address}`
+  );
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
--- test/Lock.ts
+++ test/Lock.ts
@@ -1,15 +1,9 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { loadFixture, time } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { expect } from 'chai';
+import { web3 } from 'hardhat';
+import artifacts from '../artifacts/contracts/Lock.sol/Lock.json';
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
+describe('Lock', function () {
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1_000_000_000;
@@ -17,111 +11,58 @@ describe("Lock", function () {
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const lockContract = new web3.eth.Contract(artifacts.abi);
+    lockContract.handleRevert = true;
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const [deployer, otherAccount] = await web3.eth.getAccounts();
+    const rawContract = lockContract.deploy({
+      data: artifacts.bytecode,
+      arguments: [unlockTime],
+    });
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    // To know how much gas will be consumed, we can estimate it first.
+    const estimateGas = await rawContract.estimateGas({
+      from: deployer,
+      value: lockedAmount.toString(),
+    });
+
+    const lock = await rawContract.send({
+      from: deployer,
+      gas: estimateGas.toString(),
+      gasPrice: '10000000000',
+      value: lockedAmount.toString(),
+    });
+
+    console.log('Lock contract deployed to: ', lock.options.address);
+    return { lock, unlockTime, lockedAmount, deployer, otherAccount, rawContract };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
+  describe('Deployment', function () {
+    it('Should set the right unlockTime', async function () {
       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      const setTime = await lock.methods.unlockTime().call();
+      console.log('SetTime', setTime);
+      expect(setTime).to.equal(unlockTime);
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    it('Should set the right deployer', async function () {
+      const { lock, deployer } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await lock.owner()).to.equal(owner.address);
+      expect(await lock.methods.owner().call()).to.equal(deployer);
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
+    it('Should receive and store the funds to lock', async function () {
+      const { lock, lockedAmount } = await loadFixture(deployOneYearLockFixture);
+      const balance = await web3.eth.getBalance(String(lock.options.address));
+      expect(balance).to.equal(lockedAmount);
     });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
+    it("Shouldn't fail if the unlockTime has arrived and the deployer calls it", async function () {
+      const { lock, unlockTime, deployer } = await loadFixture(deployOneYearLockFixture);
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+      await time.increaseTo(unlockTime);
+      await expect(lock.methods.withdraw().send({ from: deployer })).not.to.be.reverted;
     });
   });
 });
+
