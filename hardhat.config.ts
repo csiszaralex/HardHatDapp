fdedc6b03eed0d2c33c9adc728847d35fbc4a8ca
Csiszár Alex <csiszaralex@gmail.com>
2024-03-03 21:10:58 +0100

    :wrench: Adds some config for hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-web3-v4';
+import { HardhatUserConfig, task } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: '0.8.24',
 };
 
+task('accounts', 'Prints accounts', async (_, { web3 }) => {
+  console.log(await web3.eth.getAccounts());
+});
+
+task('accounts2', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 export default config;
+
