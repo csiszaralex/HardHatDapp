ff1985082d121d4e21f7a85a0377643e52ef4dde
Alex Csiszar <csiszaralex@gmail.com>
2024-03-25 14:21:47 +0100

    :construction: Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,23 @@
 import { ethers, web3 } from 'hardhat';
-import artifacts from '../artifacts/contracts/Lock.sol/Lock.json';
+import lockS from '../artifacts/contracts/Lock.sol/Lock.json';
 
+async function main() {
+  const counter = await ethers.deployContract('Ballot', [
+    [
+      '0x50726f706f73616c204100000000000000000000000000000000000000000000',
+      '0x50726f706f73616c204200000000000000000000000000000000000000000000',
+      '0x50726f706f73616c204300000000000000000000000000000000000000000000',
+    ],
+  ]);
+  await counter.waitForDeployment();
+  console.log(`Ballott deployed to ${counter.target}, address: ${await counter.getAddress()}`);
+
+  const ct = await ethers.deployContract('Counter');
+  await ct.waitForDeployment();
+  console.log(`Counter deployed to ${ct.target}, address: ${await ct.getAddress()}`);
+
+  // counter.proposals
+}
 async function maina() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
@@ -16,18 +33,16 @@ async function maina() {
     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
   );
 }
-async function main() {
+async function mainb() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
   const lockedAmount = web3.utils.toWei('0.001', 'ether');
   const [deployer] = await web3.eth.getAccounts();
-  const lockContract = new web3.eth.Contract(artifacts.abi);
+  const lockContract = new web3.eth.Contract(lockS.abi);
   const rawContract = lockContract.deploy({
-    data: artifacts.bytecode,
+    data: lockS.bytecode,
     arguments: [unlockTime],
   });
-
-
 
   const lock = await rawContract.send({
     from: deployer,
