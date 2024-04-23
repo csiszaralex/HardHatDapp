e838b1fdc404e2ed158736a16253d1ddd694cf4f
Alex Csiszár <csiszaralex@gmail.com>
2024-04-23 22:41:23 +0200

    Edit ballot to make less complex

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -17,10 +17,14 @@ contract Ballot {
 
     address public chairperson;
     mapping(address => Voter) public voters;
+    mapping(address => uint) public tokens;
     Proposal[] public proposals;
+    uint public end;
 
-    constructor(bytes32[] memory proposalNames) {
+
+    constructor(bytes32[] memory proposalNames, uint end) {
         chairperson = msg.sender;
+        end = end;
         voters[chairperson].weight = 1;
 
         for (uint i = 0; i < proposalNames.length; i++) {
@@ -28,91 +32,26 @@ contract Ballot {
         }
     }
 
-    // Give `voter` the right to vote on this ballot.
-    // May only be called by `chairperson`.
     function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to `false`, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use `require` to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        require(!voters[voter].voted, "The voter already voted.");
+        require(msg.sender == chairperson, 'Only chairperson can give right to vote.');
+        require(!voters[voter].voted, 'The voter already voted.');
         require(voters[voter].weight == 0);
         voters[voter].weight = 1;
     }
 
-    /// Delegate your vote to the voter `to`.
-    function delegate(address to) external {
-        // assigns reference
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "You have no right to vote");
-        require(!sender.voted, "You already voted.");
-
-        require(to != msg.sender, "Self-delegation is disallowed.");
-
-        // Forward the delegation as long as
-        // `to` also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
-
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
-        }
-
-        Voter storage delegate_ = voters[to];
-
-        // Voters cannot delegate to accounts that cannot vote.
-        require(delegate_.weight >= 1);
-
-        // Since `sender` is a reference, this
-        // modifies `voters[msg.sender]`.
-        sender.voted = true;
-        sender.delegate = to;
-
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
-            delegate_.weight += sender.weight;
-        }
-    }
-
-    /// Give your vote (including votes delegated to you)
-    /// to proposal `proposals[proposal].name`.
     function vote(uint proposal) external {
+        require(block.timestamp < end, 'Voting is closed.');
         Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "Has no right to vote");
-        require(!sender.voted, "Already voted.");
+        require(sender.weight != 0, 'Has no right to vote');
+        require(!sender.voted, 'Already voted.');
         sender.voted = true;
         sender.vote = proposal;
 
-        // If `proposal` is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
         proposals[proposal].voteCount += sender.weight;
     }
 
-    /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
     function winningProposal() public view returns (uint winningProposal_) {
+        require(block.timestamp >= end, 'Voting is not yet closed.');
         uint winningVoteCount = 0;
         for (uint p = 0; p < proposals.length; p++) {
             if (proposals[p].voteCount > winningVoteCount) {
@@ -122,10 +61,8 @@ contract Ballot {
         }
     }
 
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
     function winnerName() external view returns (bytes32 winnerName_) {
+        require(block.timestamp >= end, 'Voting is not yet closed.');
         winnerName_ = proposals[winningProposal()].name;
     }
 }
