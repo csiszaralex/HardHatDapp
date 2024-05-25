cb2679435c9d789938cb4dd7b767c7ef01ba364e
Alex Csiszár <csiszaralex@gmail.com>
2024-05-25 18:31:45 +0200

    Done backend for one vote

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,68 +1,80 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-/// @title Voting with delegation.
+import 'hardhat/console.sol';
+
 contract Ballot {
-    struct Voter {
-        uint weight;
-        bool voted;
-        address delegate;
-        uint vote;
+  struct Voter {
+    // uint weight;
+    bool voted;
+    uint vote;
+  }
+
+  struct Proposal {
+    bytes32 name;
+    uint voteCount;
+  }
+
+  error TooEarly(uint time);
+  error TooLate(uint time);
+
+  modifier onlyBeforeEnd() {
+    if (block.timestamp >= endTime) revert TooLate(endTime);
+    _;
+  }
+  modifier onlyAfterEnd() {
+    if (block.timestamp <= endTime) revert TooEarly(endTime);
+    _;
+  }
+
+  mapping(address => Voter) public voters;
+  Proposal[] public proposals;
+  bytes32 public name;
+  uint private createTime;
+  uint public startTime;
+  uint public endTime;
+
+  constructor(bytes32[] memory proposalNames,bytes32 n, uint currentTime, uint from, uint to) {
+    name = n;
+    createTime = currentTime;
+    startTime = createTime + from;
+    endTime = startTime + to;
+
+    for (uint i = 0; i < proposalNames.length; i++) {
+      proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
     }
+  }
 
-    struct Proposal {
-        bytes32 name;
-        uint voteCount;
+  function vote(uint proposal) external onlyBeforeEnd {
+    Voter storage sender = voters[msg.sender];
+    // require(sender.weight != 0, 'Has no right to vote');
+    require(!sender.voted, 'Already voted.');
+    sender.voted = true;
+    sender.vote = proposal;
+
+    proposals[proposal].voteCount += 1;
+    // proposals[proposal].voteCount += sender.weight;
+  }
+
+  function amIVoted() public view returns (bool) {
+    return voters[msg.sender].voted;
+  }
+
+  function getProposalsCount() public view returns (uint) {
+    return proposals.length;
+  }
+
+  function winningProposal() public view onlyAfterEnd returns (uint winningProposal_) {
+    uint winningVoteCount = 0;
+    for (uint p = 0; p < proposals.length; p++) {
+      if (proposals[p].voteCount > winningVoteCount) {
+        winningVoteCount = proposals[p].voteCount;
+        winningProposal_ = p;
+      }
     }
+  }
 
-    address public chairperson;
-    mapping(address => Voter) public voters;
-    mapping(address => uint) public tokens;
-    Proposal[] public proposals;
-    uint public end;
-
-
-    constructor(bytes32[] memory proposalNames, uint end) {
-        chairperson = msg.sender;
-        end = end;
-        voters[chairperson].weight = 1;
-
-        for (uint i = 0; i < proposalNames.length; i++) {
-            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
-        }
-    }
-
-    function giveRightToVote(address voter) external {
-        require(msg.sender == chairperson, 'Only chairperson can give right to vote.');
-        require(!voters[voter].voted, 'The voter already voted.');
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
-    }
-
-    function vote(uint proposal) external {
-        require(block.timestamp < end, 'Voting is closed.');
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, 'Has no right to vote');
-        require(!sender.voted, 'Already voted.');
-        sender.voted = true;
-        sender.vote = proposal;
-
-        proposals[proposal].voteCount += sender.weight;
-    }
-
-    function winningProposal() public view returns (uint winningProposal_) {
-        require(block.timestamp >= end, 'Voting is not yet closed.');
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
-            }
-        }
-    }
-
-    function winnerName() external view returns (bytes32 winnerName_) {
-        require(block.timestamp >= end, 'Voting is not yet closed.');
-        winnerName_ = proposals[winningProposal()].name;
-    }
+  function winnerName() public view onlyAfterEnd returns (bytes32 winnerName_) {
+    winnerName_ = proposals[winningProposal()].name;
+  }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,53 @@
 import { ethers, web3 } from 'hardhat';
-import lockS from '../artifacts/contracts/Lock.sol/Lock.json';
+// import {abi as BallotController} from '../artifacts/contracts/BallotController.sol/BallotController.json';
+import { ContractTransactionResponse } from 'ethers';
+import fs from 'fs';
 
 async function main() {
-  const counter = await ethers.deployContract('Ballot', [
+  const dones = { ballot: '' };
+  const ballot = await ethers.deployContract('Ballot', [
+    [
+      web3.utils.padRight(web3.utils.asciiToHex('Proposal A'), 64),
+      web3.utils.padRight(web3.utils.asciiToHex('Bela'), 64),
+      web3.utils.padRight(web3.utils.asciiToHex('Cili'), 64),
+      //0x50726f706f73616c204100000000000000000000000000000000000000000000
+      //0x50726f706f73616c2041000000000000000000000000000000000000000000
+    ],
+    web3.utils.padRight(web3.utils.asciiToHex('Who is the best?'), 64),
+    new Date().getTime(),
+    600,
+    600000,
+  ]);
+  await ballot.waitForDeployment();
+  dones.ballot = await ballot.getAddress();
+  console.log(`Ballott deployed to ${ballot.target}`);
+
+  const str = JSON.stringify(dones);
+  fs.writeFileSync('dones.json', str);
+}
+
+async function mainx() {
+  //Create a BallotController contract
+  const ballotController = ({} = await ethers.deployContract('BallotController'));
+  await ballotController.waitForDeployment();
+  await ballotController.setCreationHonor(3);
+
+  console.log(`BallotController deployed to ${ballotController.target}, and set creation honor to 3`);
+
+  //Create a Ballot with Ballott Controller
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ballot: ContractTransactionResponse = await ballotController.createBallot(
     [
       '0x50726f706f73616c204100000000000000000000000000000000000000000000',
       '0x50726f706f73616c204200000000000000000000000000000000000000000000',
       '0x50726f706f73616c204300000000000000000000000000000000000000000000',
     ],
-  ]);
-  await counter.waitForDeployment();
-  console.log(`Ballott deployed to ${counter.target}, address: ${await counter.getAddress()}`);
+    currentTimestampInSeconds + 60,
+    120,
+  );
+  console.log(ballot);
 
-  const ct = await ethers.deployContract('Counter');
-  await ct.waitForDeployment();
-  console.log(`Counter deployed to ${ct.target}, address: ${await ct.getAddress()}`);
-
-  // counter.proposals
+  // const b = await ethers.getContractAt("Ballot", ballot);
 }
 async function maina() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
@@ -28,9 +59,7 @@ async function maina() {
   });
   await lock.waitForDeployment();
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Lock with ${ethers.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`,
   );
 }
 async function mainb() {
@@ -38,28 +67,26 @@ async function mainb() {
   const unlockTime = currentTimestampInSeconds + 60;
   const lockedAmount = web3.utils.toWei('0.001', 'ether');
   const [deployer] = await web3.eth.getAccounts();
-  const lockContract = new web3.eth.Contract(lockS.abi);
-  const rawContract = lockContract.deploy({
-    data: lockS.bytecode,
-    arguments: [unlockTime],
-  });
+  // const lockContract = new web3.eth.Contract(lockS.abi);
+  // const rawContract = lockContract.deploy({
+  //   data: lockS.bytecode,
+  //   arguments: [unlockTime],
+  // });
 
-  const lock = await rawContract.send({
-    from: deployer,
-    gasPrice: '10000000000',
-    value: lockedAmount.toString(),
-  });
+  // const lock = await rawContract.send({
+  //   from: deployer,
+  //   gasPrice: '10000000000',
+  //   value: lockedAmount.toString(),
+  // });
 
-  console.log(
-    `Lock with ${web3.utils.fromWei(
-      lockedAmount,
-      'ether'
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.options.address}`
-  );
+  // console.log(
+  //   `Lock with ${web3.utils.fromWei(lockedAmount, 'ether')}ETH and unlock timestamp ${unlockTime} deployed to ${
+  //     lock.options.address
+  //   }`,
+  // );
 }
 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
